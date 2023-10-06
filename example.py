596ae867d11a69ccbb493e335faf63c683046e9d
hiddenccash <hiddenccash@gmail.com>
2023-10-06 13:01:47 -0700

    finish

--- example.py
+++ example.py
@@ -1,15 +1,5 @@
-# DynamoDB Json
-DynamoDB json util to load and dump strings of Dynamodb json format to python object and vise-versa
+from __future__ import print_function
 
-# Install
-just use pip: 
-```
-pip install dynamodb-json
-```
-# Use
-
-The dynamodb-json util works the same as json loads and dumps functions:
-```python
 import time
 import uuid
 from datetime import datetime
@@ -24,7 +14,7 @@ json_ = {"MyString": "a",
          "MyNone": None,
          "MyZero": 0,
          "myDecimal": Decimal("19.2"),  # converts Decimal to float, load it as float
-         "myLong": long(1938475658493),
+         "myLong": int(1938475658493),
          "MyNestedDict": {
              "my_other_nested": {
                  "name": "John",
@@ -37,48 +27,48 @@ json_ = {"MyString": "a",
                  "time": time.time()  # converts it to seconds python float, loads it as float
              }
          }
-    }
+         }
 
-dynamodb_json = json.dumps(json_)
-
+as_dict = False
+dynamodb_json = json.dumps(json_, as_dict=True)
+print(dynamodb_json)
 # {
-# "my_dict": {"M": {"my_date": {"S": "2017-04-22T14:41:35.780000"}}}, 
-# "MyBool": {"BOOL": false}, "MyNone": {"NULL": true}, 
+# "my_dict": {"M": {"my_date": {"S": "2017-04-22T14:41:35.780000"}}},
+# "MyBool": {"BOOL": false}, "MyNone": {"NULL": true},
 # "MyNestedDict": {
 #   "M": {"my_other_nested": {
-#       "M": {"myUUID": {"S": "2f4ad21e098f49b18e22ad209779048b"}, 
-#             "surname": {"S": "Lennon"}, "name": {"S": "John"}, 
-#             "mySet": {"L": [{"N": "1"}, {"N": "3"}, {"N": "4"}, {"N": "5"}, {"N": "6"}]}, 
-#             "floaty": {"N": "29.4"}, "time": {"N": "1492872095.78"}, 
-#             "myList": {"L": [{"N": "1"}, {"N": "3"}, {"N": "4"}, {"N": "5"}, {"N": "6"}, {"S": "This Is Sparta!"}]}, 
+#       "M": {"myUUID": {"S": "2f4ad21e098f49b18e22ad209779048b"},
+#             "surname": {"S": "Lennon"}, "name": {"S": "John"},
+#             "mySet": {"L": [{"N": "1"}, {"N": "3"}, {"N": "4"}, {"N": "5"}, {"N": "6"}]},
+#             "floaty": {"N": "29.4"}, "time": {"N": "1492872095.78"},
+#             "myList": {"L": [{"N": "1"}, {"N": "3"}, {"N": "4"}, {"N": "5"}, {"N": "6"}, {"S": "This Is Sparta!"}]},
 #             "MyOtherNone": {"NULL": true}}
 #             }
 #       }
-#   }, 
-# "myDecimal": {"N": "19.2"}, "num": {"N": "4"}, 
-# "MyString": {"S": "a"}, 
-# "myLong": {"N": "1938475658493"}, 
+#   },
+# "myDecimal": {"N": "19.2"}, "num": {"N": "4"},
+# "MyString": {"S": "a"},
+# "myLong": {"N": "1938475658493"},
 # "MyZero": {"N": "0"}
 # }
 
-
-json.loads(dynamodb_json)
+my_dict = json.loads(dynamodb_json)
+print(my_dict)
 
 # {'my_dict': {'my_date': datetime.datetime(2017, 4, 22, 14, 41, 35, 780000)}, 'MyBool': False, 'MyNone': None,
 #  'MyNestedDict': {
-#      'my_other_nested': {'myUUID': '2f4ad21e098f49b18e22ad209779048b', 
+#      'my_other_nested': {'myUUID': '2f4ad21e098f49b18e22ad209779048b',
 #                          'surname': 'Lennon', 'name': 'John',
-#                          'mySet': [1, 3, 4, 5, 6], 
-#                          'floaty': 29.4, 
+#                          'mySet': [1, 3, 4, 5, 6],
+#                          'floaty': 29.4,
 #                          'time': 1492872095.78,
-#                          'myList': [1, 3, 4, 5, 6, 'This Is Sparta!'], 
+#                          'myList': [1, 3, 4, 5, 6, 'This Is Sparta!'],
 #                          'MyOtherNone': None
 #                          }
-#              }, 
+#              },
 #  'myDecimal': 19.2,
-#  'num': 4, 
-#  'MyString': 'a', 
-#  'myLong': 1938475658493L, 
+#  'num': 4,
+#  'MyString': 'a',
+#  'myLong': 1938475658493L,
 #  'MyZero': 0
 # }
-```
